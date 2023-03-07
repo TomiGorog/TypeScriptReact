45921556d67b42371ce37f20ce03fd9611075acb
TomiGorog <tamas.peter.gorog@gmail.com>
2023-03-07 17:35:30 +0100

    typescript error fix on useref

--- message-app/src/App.tsx
+++ message-app/src/App.tsx
@@ -1,15 +1,47 @@
-import Heading from "./components/Heading"
-import Section from "./components/Section"
-import Counter from "./components/Counter"
-import List from "./components/List"
+
+import { useMemo, useRef, useState } from "react"
+
 function App() {
+  const [items, setItems] = useState([])
+  const [query, setQuery] = useState("")
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const filteredItems = useMemo(() => {
+    return items.filter(item => {
+      return item.toLowerCase().includes(query.toLowerCase())
+    })
+  }, [items, query])
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    const value = inputRef.current.value
+    if (value === "") return
+    setItems(prev => {
+      return [...prev, value]
+    })
+
+    inputRef.current.value = ""
+  }
 
   return (
     <>
-      <Heading title={"Hello"} />
-      <Section title={"Different title"} children={"children?"}></Section>
-      <Counter></Counter>
-      <List items={["Coffee", "Tacos", "Code"]} render={(item: string) => <span className="gold">{item}</span>} />
+      Search:
+      <input
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        type="search"
+      />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items:</h3>
+      {filteredItems.map(item => (
+        <div key={item}>{item}</div>
+      ))}
     </>
   )
 }
